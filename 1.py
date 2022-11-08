93376d8b2a874d462dd63db8b2d1d0b1a0add17b
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-11-08 11:35:32 +0800

    Task 2.1 исправленная

--- 1.py
+++ 1.py
@@ -4,11 +4,22 @@ def task_1(a, b, c) -> tuple:
     return (a, b, c)
 
 
-print(task_1(1, 2, 3))
+# print(task_1(1, 2, 3))
 
 
-def task_2(a, b):
-    if isinstance(a, (int, float)) or isinstance(b, (int, float)):
-        return a+b
-    else:
-        raise TypeError
+def task_2():
+    while True:
+        a, b = input().split()
+        try:
+            a = float(a)
+            b = float(b)
+        except ValueError:
+            continue
+        else:
+            break
+
+    return a+b
+
+
+# print(task_2(1.2, 2))
+def task_2_2(*args):
