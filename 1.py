e529ff34aa468a409678681b4b989fc658c8ee99
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2023-01-10 10:29:01 +0800

    finally

--- 1.py
+++ 1.py
@@ -1,79 +1,135 @@
+import datetime
+import math
+import time
+from random import randint
 
-def task_1(a, b, c) -> tuple:
+from memory_profiler import profile
+
+
+@profile
+def task_1() -> tuple:
+    start = datetime.datetime.now()
+    a, b, c = input("три значения через пробел: ").split()
     a, b, c = b, c, a
-    return (a, b, c)
+    end = datetime.datetime.now()
+    print(f"Время затраченное на task_1 {end-start}")
+    print(a, b, c)
 
 
-# print(task_1(1, 2, 3))
+print("TASK_1")
+task_1()
 
 
 def task_2():
     while True:
-        a, b = input().split()
+        a, b = input("Введите два числа: ").split()
         try:
             a = float(a)
             b = float(b)
         except ValueError:
+            print("Ошибка")
             continue
         else:
             break
 
-    return a+b
+    print(a+b)
 
 
-# print(task_2(1.2, 2))
+print("TASK_2")
+task_2()
+
+
 def task_2_2():
     numbers = []
     N = int(input("Введите колво чисел"))
     while N > 0:
         a = input()
-
         try:
             a = float(a)
             numbers.append(a)
         except ValueError:
-            N += 1
-
+            continue
         else:
             N -= 1
-    print(sum(numbers))
+    print("сумма= ", sum(numbers))
     return sum(numbers)
 
 
+print("TASK_2_2")
+task_2_2()
+
+
+@profile
 def task_3_1(x):
-    return x**5
+    start = datetime.datetime.now()
+    print(x**5)
+    end = datetime.datetime.now()
+    print(f"Затраченное время: {end-start}")
 
 
+print("TASK_3_1 c значением 5:")
+task_3_1(5)
+print()
+
+
+@profile
 def task_3_2(x):
+    start = datetime.datetime.now()
     out = 1
     for i in range(5):
         out *= x
+    end = datetime.datetime.now()
+    print(f"Затраченное время: {end-start}")
     return out
 
 
-# x = 2
-# print(task_3_2(x))
+print("TASK_3_2 c значением 5:")
+print(task_3_2(5))
+print()
 
-# print(task_3_1(x))
 
 def task_4(x: int) -> bool:
+    i = 0
+    a = 1
+    fibonachi = []
+    while i <= 250:
+        i += a
+        a = i - a
+        fibonachi.append(i)
     if x <= 250:
-        fibonachi = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
         if x in fibonachi:
             return True
         return False
 
 
+print("TASK_4:")
+x = int(input("введите число: "))
+print(task_4(x))
+print()
+
+
 def task_5(month: int):
+    dict_moth = {'Зима': (1, 2, 12),
+                 'Весна': (3, 4, 5),
+                 'Лето': (6, 7, 8),
+                 'Осень': (9, 10, 11)}
+    print([key for key in dict_moth.keys() if month in dict_moth[key]])
+
     num = month
-    if num <= 2:
-        return "winter"
-    if num <= 5:
-        return "spring"
-    if num <= 8:
-        return "summer"
-    if num <= 11:
-        return "autumn"
+    if (num <= 2) or (num == 12):
+        return "Зима"
+    if (num >= 3) and (num <= 5):
+        return "Весна"
+    if (num >= 6) and (num <= 8):
+        return "Лето"
+    if (num >= 9) and (num <= 11):
+        return "Осень"
+
+
+print("TASK_5:")
+x = int(input("введите число: "))
+task_5(x)
+print()
 
 
 def task_6(N):
@@ -90,18 +146,50 @@ def task_6(N):
     return sum, even, not_even
 
 
+print("TASK_6:")
+x = int(input("введите число: "))
+print(task_6(x))
+print()
+
+
+@profile
 def task_7(N):
     def del_search(num):
         delit = 1
         output = set()
+        # проходит только до корня, записывает парный делитель
         while delit <= num**0.5:
             if num % delit == 0:
                 output.add(delit)
                 output.add(num//delit)
             delit += 1
         return output
+    start = datetime.datetime.now()
     for i in range(1, N+1):
         print(i, del_search(i))
+    end = datetime.datetime.now()
+    print(f"Затраченное время: {end-start}")
+
+
+print("TASK_7:")
+x = int(input("введите число: "))
+task_7(x)
+print()
+
+
+@profile
+def task_8():
+    N, M = map(int, input("Введите N и M: ").split())
+    for b in range(N, M):
+        for a in range(N, b):
+            c = math.sqrt(a * a + b * b)
+            if c % 1 == 0:
+                return a, b, int(c)
+
+
+print("TASK_8:")
+print(task_8())
+print()
 
 
 def task_9(N, M):
@@ -116,35 +204,85 @@ def task_9(N, M):
             print(i)
 
 
-# task_9(10, 25)
+print("TASK_9:")
+N = int(input("Введите N: "))
+M = int(input("Введите M: "))
+task_9(N, M)
+print()
 
 
 def task_10(N):
-    def del_search(num):
-        delit = 1
-        output = set()
-        while delit <= num**0.5:
-            if num % delit == 0:
-                output.add(delit)
-                output.add(num//delit)
-            delit += 1
-        return output
+    def del_search(a):
+        if a == 1:
+            return 1
+        sum_num = 0
+        for i in range(1, a // 2 + 1):
+            if a % i == 0:
+                sum_num += i
+        return sum_num
 
-    num = 1
-    output = []
-    while len(output) < N:
-        flag = True
-        for deli in del_search(num):
-            print(del_search(num))
-            print("1", deli)
-            if num % deli != 0:
-                flag = False
-                break
-        if flag:
-            print(num)
-            output.append(num)
-        num += 1
-    return output
+    N, i = 1, 1
+    f = []
+    while N < 5:
+        i += 1
+        if i == del_search(i):
+            f.append(i)
+            N += 1
+    return f
 
 
-print(task_10(3))
+print("TASK_10: для 4 чисел")
+print(task_10(4))
+print()
+
+
+def task_11():
+    mac = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    start1 = time.time()
+    print(mac[-1])
+    end1 = time.time()
+
+    start2 = time.time()
+    print(next(reversed(mac)))
+    end2 = time.time()
+
+    start3 = time.time()
+    print(mac.pop())
+    end3 = time.time()
+
+    print(
+        f"срез: {start1 - end3}, reversed: {start2 - end2}, pop: {start3 - end3}")
+
+
+print("TASK_11")
+task_11()
+print()
+
+
+def task_12():
+    mac = [1, 2, 3, 4, 5]
+    return mac[::-1]
+
+
+print("TASK_12")
+print(task_12())
+print()
+
+
+def task_13():
+    def sum_list(l, res, i=0):
+        if i == len(l):
+            return res
+        else:
+            res += l[i]
+            i += 1
+            return sum_list(l, res, i)
+
+    list_num = [1, 2, 3, 4, 5]
+    print(sum_list(list_num, 0))
+
+
+print("TASK_13")
+task_13()
+print()
